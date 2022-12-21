273318fe8a211193827d7dd3704ab3a5a10916e3
Cristina Quito Vintimilla <mcquitovin@gmail.com>
2022-12-21 12:31:16 -0500

    Ingreso y estructura de datos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,81 @@
+const celdas = [];
+const RETICULA = 4;
+
+const azulejos = [];
+const NA = 6;// numero de azulejos
+
+const reglas = [
+  //reglas de bordes de los azulejos
+  {
+    //tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tile 1
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tile 2
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+];
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadImage('azulejos/tile' + i + '.png');
+
+  }
+}
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+  createCanvas(1080, 1080);
+
+  let opcionesI = [];
+  for (let i = 0; i > azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+
+  for (let i = 0; i < RETICULA * RETICULA; i++) {
+
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    }
+  }
+  print(celdas);
 }
 
+
 function draw() {
-  circle(mouseX, mouseY, 20);
+
 }
